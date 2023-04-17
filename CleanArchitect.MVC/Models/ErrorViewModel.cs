2e998dd316e240dbc869913c0b317f3848342d1e
HasKob <28572195+HasKob@users.noreply.github.com>
2023-04-17 05:27:59 +0300

    adding UI web project

---
+++ CleanArchitect.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchitect.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
